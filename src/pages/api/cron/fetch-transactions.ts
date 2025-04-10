d5d6de53e895514474a5e274c9dc2fe4bc3ab27e
andrejdergavko <andrejdergavko@gmail.com>
2025-04-10 12:21:45 +0300

    Update console.log value in fetch-transactions cron handler

--- src/pages/api/cron/fetch-transactions.ts
+++ src/pages/api/cron/fetch-transactions.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(111111111);
+  console.log(111111111111);
   res.status(200).end('Hello Cron!');
 }
 
